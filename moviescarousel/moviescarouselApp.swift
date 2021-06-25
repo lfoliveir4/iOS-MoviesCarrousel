6559b640ad4e138411e3a2d85ab3b3c693823d9d
Luis Filipe Alves de Oliveira <lfoliveira.dev@gmail.com>
2021-06-24 22:01:40 -0300

    Initial Commit

---
+++ moviescarousel/moviescarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviescarouselApp.swift
+//  moviescarousel
+//
+//  Created by Luis Filipe Alves de Oliveira on 24/06/21.
+//
+
+import SwiftUI
+
+@main
+struct moviescarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
